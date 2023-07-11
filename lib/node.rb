16a501575e51438486fff68135e17d2b8d09c1e3
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-07-11 04:49:49 -0400

    Test for Node Class

--- lib/node.rb
+++ lib/node.rb
@@ -1,2 +1,8 @@
-
-
+class Node 
+    attr_reader:data
+    def initialize(data)
+        @data=data 
+        #@next_node=next_node 
+    end 
+end 
+   
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,23 +1,12 @@
-require "./lib/node"
-
-# RSpec.describe Node do
-#   describe '#initialize' do
-#   it "is an instance of node" do 
-#     node=Node.new("plop")
-#     expect(node).to eq(Node)
-#   end 
-Rspec.describe Node do 
-  xit "has a data" do 
+require './lib/node'
+require 'rspec'
+RSpec.describe Node do 
+  describe '#initialize' do
+    it "is an instance of Node" do 
     node=Node.new("plop")
     expect(node.data).to eq("plop")
-  end
+    end 
+  end 
+end 
 
-  xit "has data" do 
-    node = Node.new("plop")
-    expect(node.data).to eq("plop")
-  #end 
 
-  xit "It has a method next_node" do 
-    node = Node.new("plop")
-    expect(node.next_node).to eq(nil)
-  
